95f2c533ce7a2ee6ec25b77a9992bfa0d1b001c0
tucinat <tucinathalie@gmail.com>
2023-10-29 19:25:15 -0500

    3ème événment js + modif du favicon, creation des liens ancres

--- js/index.js
+++ js/index.js
@@ -60,7 +60,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
- //Pour la ligne popup Jordan
+  //Pour la ligne popup Jordan
 
  document.addEventListener("DOMContentLoaded", function () {
   const openPopupButton = document.getElementById("openPopupButtonJ");
@@ -75,6 +75,81 @@ document.addEventListener("DOMContentLoaded", function () {
       popup.style.display = "none";
   });
 });
+
+
+
+//3ÈME ÉVÉNEMENT : événement qui permet de s'inscrire à l'infolettre
+
+document.addEventListener("DOMContentLoaded", function () {
+  const popupTrigger = document.getElementById("popupTrigger");
+  const popup = document.getElementById("popupscroll");
+  const popupClose = document.getElementById("popupClose");
+
+  popupTrigger.addEventListener("click", function () {
+      popup.style.display = "block";
+  });
+
+  popupClose.addEventListener("click", function () {
+      popup.style.display = "none";
+  });
+
+  window.addEventListener("scroll", function () {
+      if (window.scrollY > 3000) {
+          // Faites défiler vers le bas pour ouvrir la popup
+          popup.style.display = "block";
+      }
+
+  });
+
+});
+
+  // validation js qui provient de bootstrap pour le formulaire de l'infolettre
+  (function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+
+  // Ouverture d une seconde popup pour confirmer l'inscription
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const openPopupButton = document.getElementById("openPopupButtonI");
+  const confirmationPopup = document.getElementById("confirmation");
+  const popupCloseInscription = document.getElementById("popupCloseInscription");
+
+  openPopupButton.addEventListener("click", function () {
+      confirmationPopup.style.display = "block"; // Afficher la deuxième popup
+      popup.style.display = "none"; // Masquer la première popup de l'infolettre
+      
+  });
+
+  popupCloseInscription.addEventListener("click", function () {
+      confirmationPopup.style.display = "none"; // Masquer la deuxième popup
+  });
+});
+
+
+
+
+
+
 
 
 
